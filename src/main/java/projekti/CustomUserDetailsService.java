3bf2b91ac17305ef9deabb7e6ddbb1109fdfff5a
sailaja2003 <99901787+sailaja2003@users.noreply.github.com>
2023-04-26 20:26:11 +0530

    Update CustomUserDetailsService.java

--- src/main/java/projekti/CustomUserDetailsService.java
+++ src/main/java/projekti/CustomUserDetailsService.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package projekti;
 
-/**
- *
- * @author piaandersin
- */
+
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
